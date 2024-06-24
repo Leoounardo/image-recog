67718d5013f67c855f50c5b9d441012678c61116
Leonardo <leotavaresmj@gmail.com>
2024-06-24 17:33:20 -0400

    new imshow and some changes in for epoch loop
    acc 75% - horrorosu

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import torch
-import torchvision
+ 
 import torchvision.transforms as transforms
+from torchvision import models, datasets, utils
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
 
+import torch.optim as optim
 data_path = 'dataset/'
 
 transform = transforms.Compose(
     [
      transforms.RandomResizedCrop(224),
      transforms.RandomHorizontalFlip(),
+     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+     transforms.Grayscale(),
      transforms.ToTensor(),
-     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])     ])
+     transforms.Normalize([0.485], [0.229])
+     ])
+
+val_transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
 
 batch_size = 32
 
-classes = ('bone', 'fractured')
+classes = ('fractured', 'not fractured')
 
-trainset = torchvision.datasets.ImageFolder(root=data_path+"train/", transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=4)
+trainset = datasets.ImageFolder(root=data_path+"train/", transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=6)
 
-testset = torchvision.datasets.ImageFolder(root=data_path+"test/", transform=transform)
+testset = datasets.ImageFolder(root=data_path+"test/", transform=val_transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
 
-import matplotlib.pyplot as plt
-import numpy as np
- 
+valset = datasets.ImageFolder(root=data_path+"val/", transform=val_transform)
+valloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+
 # functions to show an image
-def imshow(img):
+def imshow(img, title = None):
     img = img     # unnormalize
     npimg = img.numpy()
     print(npimg)
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    if title != None:
+        plt.title(title)
     plt.show()
 
 from PIL import Image, ImageFile
@@ -41,26 +60,23 @@ def pil_loader(path):
         try:
             with open(path, 'rb') as f:
                 img = Image.open(f)
-                return img.convert('RGB')
+                return img.convert('L')
         except (OSError, UnidentifiedImageError) as e:
             print(f"Warning: {e} - {path}. Retrying...")
     # Se falhar em todas as tentativas, retornar uma imagem preta (ou você pode escolher outra abordagem)
-    return Image.new('RGB', (224, 224))
-
-import torch.nn as nn
-import torch.nn.functional as F
+    return Image.new('L', (224, 224))
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, padding='same')
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding='same')
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding='same')
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding='same')
         self.fc1 = nn.Linear(in_features=32 * 56 * 56, out_features=128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc3 = nn.Linear(64, 2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -84,52 +100,95 @@ class Net(nn.Module):
         x = self.fc3(x)
 
         return x
-
-import torch.optim as optim
+    
+def imshow2(images, labels, class_names, fontsize=10, ncol=7, nrow=5):
+    num_images = len(images)
+    assert num_images <= ncol * nrow, "Número de imagens excede a capacidade da grade."
+    
+    fig, axes = plt.subplots(nrow + 1, ncol, figsize=(15, 15))  # +1 para títulos
+    axes = axes.flatten()  # Achata a grade de eixos para fácil iteração
+    
+    for col in range(ncol):
+        if col < len(labels):
+            axes[col].set_title(class_names[labels[col]], fontsize=fontsize)
+        axes[col].axis('off')  # Desativa os eixos para os títulos
+    
+    for i, (img, label, ax) in enumerate(zip(images, labels, axes[ncol:])):
+        img = img / 2 + 0.5  # unnormalize
+        npimg = img.numpy()
+        ax.imshow(np.transpose(npimg, (1, 2, 0)), cmap='gray')
+        ax.axis('off')
+    
+    # Remove os eixos excedentes se houver menos imagens do que subplots
+    for ax in axes[num_images+ncol:]:
+        ax.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
-
-    # dataiter = iter(trainloader)
-    # images, labels = next(dataiter)
-
-    # print(images.shape)
-    # print(labels.shape)
-    # images, labels = next(dataiter)
-
-    # imshow(torchvision.utils.make_grid(images))
-    # print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-    
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
+    class_names = trainset.classes
+    imshow2(images, labels, class_names, fontsize=8)
+    print(images.shape)
+    print(labels.shape)
+ 
     net = Net()
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) 
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(device)
-    net.to(device)
-    net.train()
-
+    # model = model.to(device)
+    net = net.to(device)
+    
     print("chegamos até aqui")
-    for epoch in range(50):  # loop no dataset multiplas vezes
-
-        running_loss = 0.0
+    num_epochs = 20
+    for epoch in range(num_epochs):  
+        net.train()
+        train_loss = 0
+        train_correct = 0
         for i, data in enumerate(trainloader, start=0):
             # obter as entradas; data é uma lista de [inputs, labels]
             inputs, labels = data[0].to(device), data[1].to(device)
 
-            # zerar o gradiente dos parâmetros
             optimizer.zero_grad()
-
-            # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
-            # imprimindo estatísticas
-            running_loss += loss.item()
-            if (i+1) % 500 == 0:    # print a cada 2000
-                print(f'[Época: {epoch + 1}, iter: {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
+            train_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            train_correct += (predicted == labels).sum().item()
+
+        train_loss /= len(trainloader)
+        train_accuracy = train_correct / len(trainloader.dataset)
+
+        net.eval()
+        val_loss = 0
+        val_correct = 0
+        with torch.no_grad():
+            for images, labels in valloader:
+                try:
+                    images, labels = images.to(device), labels.to(device)
+                    outputs = net(images)
+                    loss = criterion(outputs, labels)
+                    val_loss += loss.item()
+                    _, predicted = torch.max(outputs, 1)
+                    val_correct += (predicted == labels).sum().item()
+                except Exception as e:
+                    print(f"Error processing batch: {e}")
+                    continue
+
+        val_loss /= len(valloader)
+        val_accuracy = val_correct / len(valloader.dataset)
+        
+        scheduler.step()
+
+        print(f'{epoch+1}/{num_epochs} | Train Loss: {train_loss:.4f}, Train Acc: {train_accuracy:.4f}, '
+              f'Val Loss: {val_loss:.4f}, Val Acc: {val_accuracy:.4f}')
 
     print('Finished Training')
     # PATH = '/savetest'
@@ -138,7 +197,7 @@ if __name__ == '__main__':
     images, labels = next(dataiter)
 
     # print images
-    imshow(torchvision.utils.make_grid(images))
+    imshow(utils.make_grid(images))
     print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
 
     images, labels = images.to(device), labels.to(device)
